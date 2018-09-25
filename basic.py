7d3855aa2fcda7275b549543c7b4b04c0d65f2d0
nnhw <nnhw667@gmail.com>
2018-09-25 19:44:19 +0300

    takeoff for plane with preloaded mission

--- basic.py
+++ basic.py
@@ -5,62 +5,89 @@ from dronekit import connect, VehicleMode, APIException
 import socket
 import exceptions
 from sys import exit
-
-# Set up option parsing to get connection string
 import argparse
+import time
+
 parser = argparse.ArgumentParser(
     description='DroneKit experiments.')
 parser.add_argument('--connect',
                     help="vehicle connection target string. If not specified, script connects to 127.0.0.1:14550 by default.")
 args = parser.parse_args()
 
-connection_string = args.connect
+def safe_connect():
+    connection_string = args.connect
 
-if not connection_string:
-    connection_string = "127.0.0.1:14550"
+    if not connection_string:
+        connection_string = "127.0.0.1:14551"
 
-# Connect to the Vehicle.
-print("Connecting to vehicle on: %s" % (connection_string,))
+    # Connect to the Vehicle.
+    print("Connecting to vehicle on: %s" % (connection_string,))
 
-try:
-    vehicle = connect(connection_string, heartbeat_timeout=15, wait_ready=True)
+    try:
+        vehicle = connect(connection_string, heartbeat_timeout=15, wait_ready=True)
 
-# Bad TCP connection
-except socket.error:
-    print ('No server exists!')
-    exit(1)
+    # Bad TCP connection
+    except socket.error:
+        print ('No server exists!')
+        exit(1)
 
-# Bad TTY connection
-except exceptions.OSError as e:
-    print ('No serial exists!')
-    exit(1)
+    # Bad TTY connection
+    except exceptions.OSError as e:
+        print ('No serial exists!')
+        exit(1)
 
-# API Error
-except APIException:
-    print ('Timeout!')
-    exit(1)
+    # API Error
+    except APIException:
+        print ('Timeout!')
+        exit(1)
 
-# Other error
-except:
-    print ('Some other error!')
-    exit(1)
+    # Other error
+    except:
+        print ('Some other error!')
+        exit(1)
 
-# Don't try to arm until autopilot is ready
-while not vehicle.is_armable:
-    print(" Waiting for vehicle to initialise...")
-    time.sleep(1)
+    return vehicle
 
-# Get some vehicle attributes (state)
-print("Get some vehicle attribute values:")
-print(" GPS: %s" % vehicle.gps_0)
-print(" Battery: %s" % vehicle.battery)
-print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
-print(" Is Armable?: %s" % vehicle.is_armable)
-print(" System status: %s" % vehicle.system_status.state)
-print(" Mode: %s" % vehicle.mode.name)    # settable
+def arm_and_takeoff(aVehicle,aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not aVehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Plane should arm first
+    aVehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not aVehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    # Setting mode to AUTO
+    aVehicle.mode = VehicleMode("AUTO")
+    print("Taking off!")
+    # aVehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
+    while True:
+        print(" Altitude: ", aVehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if aVehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+vehicle = safe_connect()
+arm_and_takeoff(vehicle,50);
 
 # Close vehicle object before exiting script
 vehicle.close()
 
-# Shut down simulator
 print("Completed")
