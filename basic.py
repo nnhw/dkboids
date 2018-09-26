d207599f431eb1c52815870bc09bc2a693fefe1c
nnhw <nnhw667@gmail.com>
2018-09-26 20:13:04 +0300

    Added attitude control for collision avoidance

--- basic.py
+++ basic.py
@@ -8,12 +8,14 @@ import exceptions
 from sys import exit
 import argparse
 import time
+import math
 
 parser = argparse.ArgumentParser(
     description='DroneKit experiments.')
 parser.add_argument('--connect',
                     help="vehicle connection target string. If not specified, script connects to 127.0.0.1:14550 by default.")
 args = parser.parse_args()
+
 
 def safe_connect():
     connection_string = args.connect
@@ -25,7 +27,8 @@ def safe_connect():
     print("Connecting to vehicle on: %s" % (connection_string,))
 
     try:
-        vehicle = connect(connection_string, heartbeat_timeout=15, wait_ready=True)
+        vehicle = connect(connection_string,
+                          heartbeat_timeout=15, wait_ready=True)
 
     # Bad TCP connection
     except socket.error:
@@ -49,7 +52,8 @@ def safe_connect():
 
     return vehicle
 
-def arm_and_takeoff(aVehicle,aTargetAltitude):
+
+def arm_and_takeoff(aVehicle, aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
     """
@@ -58,10 +62,11 @@ def arm_and_takeoff(aVehicle,aTargetAltitude):
     cmds = vehicle.commands
 
     print(" Clear any existing commands")
-    cmds.clear() 
+    cmds.clear()
 
-    #Add MAV_CMD_NAV_TAKEOFF command. This is ignored if the vehicle is already in the air.
-    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0, 50))
+    # Add MAV_CMD_NAV_TAKEOFF command. This is ignored if the vehicle is already in the air.
+    cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                     mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0, 50))
     print(" Upload new commands to vehicle")
     cmds.upload()
 
@@ -96,19 +101,75 @@ def arm_and_takeoff(aVehicle,aTargetAltitude):
             break
         time.sleep(1)
 
+
 def download_mission():
     """
     Download the current mission from the vehicle.
     """
     cmds = vehicle.commands
     cmds.download()
-    cmds.wait_ready() # wait until download is complete.
+    cmds.wait_ready()  # wait until download is complete.
+
+
+def set_attitude(roll_angle=0.0, pitch_angle=0.0, yaw_rate=0.0, thrust=0.5, duration=0):
+    """
+    Note that from AC3.3 the message should be re-sent every second (after about 3 seconds
+    with no message the velocity will drop back to zero). In AC3.2.1 and earlier the specified
+    velocity persists until it is canceled. The code below should work on either version
+    (sending the message multiple times does not cause problems).
+    """
+
+    """
+    The roll and pitch rate cannot be controllbed with rate in radian in AC3.4.4 or earlier,
+    so you must use quaternion to control the pitch and roll for those vehicles.
+    """
+
+    # Thrust >  0.5: Ascend
+    # Thrust == 0.5: Hold the altitude
+    # Thrust <  0.5: Descend
+    msg = vehicle.message_factory.set_attitude_target_encode(
+        0,  # time_boot_ms
+        1,  # Target system
+        1,  # Target component
+        0b00000000,  # Type mask: bit 1 is LSB
+        to_quaternion(roll_angle, pitch_angle),  # Quaternion
+        0,  # Body roll rate in radian
+        0,  # Body pitch rate in radian
+        math.radians(yaw_rate),  # Body yaw rate in radian
+        thrust  # Thrust
+    )
+    vehicle.send_mavlink(msg)
+
+    start = time.time()
+    while time.time() - start < duration:
+        vehicle.send_mavlink(msg)
+        time.sleep(0.1)
+
+
+def to_quaternion(roll=0.0, pitch=0.0, yaw=0.0):
+    """
+    Convert degrees to quaternions
+    """
+    t0 = math.cos(math.radians(yaw * 0.5))
+    t1 = math.sin(math.radians(yaw * 0.5))
+    t2 = math.cos(math.radians(roll * 0.5))
+    t3 = math.sin(math.radians(roll * 0.5))
+    t4 = math.cos(math.radians(pitch * 0.5))
+    t5 = math.sin(math.radians(pitch * 0.5))
+
+    w = t0 * t2 * t4 + t1 * t3 * t5
+    x = t0 * t3 * t4 - t1 * t2 * t5
+    y = t0 * t2 * t5 + t1 * t3 * t4
+    z = t1 * t2 * t4 - t0 * t3 * t5
+
+    return [w, x, y, z]
+
 
 vehicle = safe_connect()
 
 if vehicle.groundspeed < 0.1:
     print("Arming and taking-off!")
-    arm_and_takeoff(vehicle,50);
+    arm_and_takeoff(vehicle, 50)
     print("Set default/target airspeed to 30")
     vehicle.groundspeed = 30
 else:
@@ -116,6 +177,7 @@ else:
 
 vehicle.mode = VehicleMode("GUIDED")
 
+set_attitude(thrust=0, pitch_angle = 10, duration=10)
 
 # print("Returning to Launch")
 # vehicle.mode = VehicleMode("RTL")
